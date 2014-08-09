0a2a8e7deeaca2aeeeee543c7aa94b57d4729edd
brijeshpatil <brijesh.patil@tops-int.com>
2014-08-09 11:51:51 +0530

    Uploaded Files

---
+++ ImageHandler.ashx
@@ -0,0 +1,28 @@
+﻿<%@ WebHandler Language="C#" Class="ImageHandler" %>
+
+using System;
+using System.Web;
+
+public class ImageHandler : IHttpHandler {
+
+    System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+    public void ProcessRequest(HttpContext context)
+    {
+        string imageid = context.Request.QueryString["ImID"];
+        con.Open();
+        System.Data.SqlClient.SqlCommand command = new System.Data.SqlClient.SqlCommand("select uimage from imgtbl where imageid=" + imageid, con);
+        System.Data.SqlClient.SqlDataReader dr = command.ExecuteReader();
+        dr.Read();
+        context.Response.BinaryWrite((Byte[])dr[0]);
+        con.Close();
+        context.Response.End();
+    }
+ 
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+   
+
+}
---
+++ UploadExample.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UploadExample : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindGridData();
+        }
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        if (fileuploadImage.HasFile)
+        {
+            int length = fileuploadImage.PostedFile.ContentLength;
+            byte[] imgbyte = new byte[length];
+            HttpPostedFile img = fileuploadImage.PostedFile;
+            img.InputStream.Read(imgbyte, 0, length);
+            string imagename = txtImageName.Text;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO imgtbl (imagename,uimage) VALUES (@imagename,@imagedata)", con);
+            cmd.Parameters.Add("@imagename", SqlDbType.VarChar, 50).Value = imagename;
+            cmd.Parameters.Add("@imagedata", SqlDbType.Image).Value = imgbyte;
+            int count = cmd.ExecuteNonQuery();
+            con.Close();
+            if (count == 1)
+            {
+                BindGridData();
+                txtImageName.Text = string.Empty;
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "alertmessage", "javascript:alert('" + imagename + " image inserted successfully')", true);
+            }
+        }
+    }
+
+    private void BindGridData()
+    {
+        SqlCommand command = new SqlCommand("SELECT imagename,ImageID from [imgtbl]", con);
+        SqlDataAdapter daimages = new SqlDataAdapter(command);
+        DataTable dt = new DataTable();
+        daimages.Fill(dt);
+        gvImages.DataSource = dt;
+        gvImages.DataBind();
+        gvImages.Attributes.Add("bordercolor", "black");
+    }
+}
